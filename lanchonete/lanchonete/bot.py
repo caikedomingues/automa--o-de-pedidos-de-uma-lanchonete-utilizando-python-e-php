81b24098c498399f01958d71b5a2ac64a441c021
Caike De Oliveira Domingues <caike.dom@gmail.com>
2026-02-18 16:44:19 -0300

    criei uma parte da logica de criação e recebimento de mensagens usando o botcity
    Posteriormente, terei que pensar ou pesquisar na documentação uma maneira dele sempre pegar a ultima mensagem do usuário com o objetivo de validar o pedido do cliente

--- lanchonete/lanchonete/bot.py
+++ lanchonete/lanchonete/bot.py
@@ -1,67 +1,46 @@
-"""
-WARNING:
 
-Please make sure you install the bot with `pip install -e .` in order to get all the dependencies
-on your Python environment.
-
-Also, if you are using PyCharm or another IDE, make sure that you use the SAME Python interpreter
-as your IDE.
-
-If you get an error like:
-```
-ModuleNotFoundError: No module named 'botcity'
-```
-
-This means that you are likely using a different Python interpreter than the one used to install the bot.
-To fix this, you can either:
-- Use the same interpreter as your IDE and install your bot with `pip install -e .`
-- Use the same interpreter as the one used to install the bot (`pip install -e .`)
-
-Please refer to the documentation for more information at https://documentation.botcity.dev/
-"""
-
-"""
-********************************************************************************
-*                                   WARNING                                    *
-********************************************************************************
-
-You are using an outdated version of the template which is no longer recommended.
-While this template works and is valid, it is strongly recommended to create a new project with the latest template version.
-
-Please refer to our documentation: https://documentation.botcity.dev/tutorials/python-automations/desktop/
-"""
 from botcity.core import DesktopBot
-# Uncomment the line below for integrations with BotMaestro
-# Using the Maestro SDK
-# from botcity.maestro import *
 
 
 class Bot(DesktopBot):
     def action(self, execution=None):
-        # Uncomment to silence Maestro errors when disconnected
-        # if self.maestro:
-        #     self.maestro.RAISE_NOT_CONNECTED = False
+      
+       while True:  
+            # Searching for element 'nova_mensagem '
+            if self.find("nova_mensagem", matching=0.97, waiting_time=10000):
+                    print("nova mensagem encontrada")
+                    
+                    self.click()
+                            
+                    self.paste("Ola, por favor, digite sua mensagem aqui")
+                            
+                    # Searching for element 'enviar_mensagem '
+                    if self.find("enviar_mensagem", matching=0.97, waiting_time=10000):
+                            print("Mensagem Enviada")
+                            self.click()
+                    
+                    # Searching for element 'resposta_cliente '
+                    if self.find("resposta_cliente", matching=0.97, waiting_time=10000):
+                        
+                        pass
 
-        # Fetch the Activity ID from the task:
-        # task = self.maestro.get_task(execution.task_id)
-        # activity_id = task.activity_id
+                        
+                    
+                    
 
-        # Opens the BotCity website.
-        self.browse("www.youtube.com")
-
-        # Uncomment to mark this task as finished on BotMaestro
-        # self.maestro.finish_task(
-        #     task_id=execution.task_id,
-        #     status=AutomationTaskFinishStatus.SUCCESS,
-        #     message="Task Finished OK.",
-        #     total_items=0,
-        #     processed_items=0,
-        #     failed_items=0
-        # )
-
+       
     def not_found(self, label):
         print(f"Element not found: {label}")
 
 
 if __name__ == '__main__':
     Bot.main()
+
+
+
+
+
+
+
+
+
