dd6a021a51330982202ee809cc8298bba441ff77
Caike De Oliveira Domingues <caike.dom@gmail.com>
2026-03-13 12:17:28 -0300

    comentei o codigo da conexão com o banco de dados

--- classes/BancoDeDados.php
+++ classes/BancoDeDados.php
@@ -3,29 +3,49 @@
 
 <?php
 
-    # Este arquivo ira conter todas as operações que queremos realizar no banco de dados.    
-    # Vamos usar  
+    #Este aequivo ira conter a conexão com o banco de dados que iremos utilizar em outras
+    # classes com o objetivo de conectar os métodos de cada objeto ao banco de dados.
+    # Vamos usar essa classe parav realizar a injeção de dependência que basicamente 
+    # consiste em tornar uma classe dependente da existência da outra, ou seja, todos
+    # os metodos das outra classes irão depender do metodo conexao da classe BancoDeDados.
     
+    # Criação da classe banco de dados que conterá o método de conexão.
     class BancoDeDados{
 
+        # método que irá criar a conexão com o banco de dados
         public function conexaoBanco(){
 
+        # Ira inspecionar o bloco de código com o objetivo de capturar possiveis
+        # erros de execução do código. 
         try{
 
+            # dsn é o link que contém as informações necessárias do banco de dados
+            # como o endereço do servidor e o nome do banco de dados.
             $dsn = "mysql:host=localhost;dbname=lanchonete;charset=utf8mb4";
 
+            # Ira conter o usuário que administra o banco de dados.
             $usuario = "root";
 
+            # Ira conter a senha do banco de dados.
             $senha = "";
 
+            # Ira instanciar a classe PDO que cria a conexão com um banco de dados mysql.
+            # A classe recebe em seu construtor o dsn(link com os dados do servidor), o
+            # usuario e a senha do banco.
             $conexao = new PDO($dsn, $usuario, $senha);
 
+            # Mensagem que ira indicar o sucesso da execução do metodo.
             echo "Conexão realizada";
 
+            # Ira retornar a conexão com o banco de dados (valores que irão
+            # conectar as classes ao banco).
             return $conexao;
 
         }catch(PDOException $erro){
-
+                
+                # Ira lidar com erros relacionados as operações realizadas no banco de dados.
+                # O getMessage é uma função que tem como objetivo retornar a mensagem do erro
+                # gerado pela exceção
                 echo "erro na conexao: " . $erro->getMessage();
         }
       }
